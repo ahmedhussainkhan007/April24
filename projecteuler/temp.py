a19ca120c583f86b8d324e505a351fa35e0a35cb
ahmedhussainkhan <ahmedhussainkhan018@gmail.com>
2024-05-07 14:08:30 +0530

    added changes

---
+++ projecteuler/temp.py
@@ -0,0 +1,42 @@
+number = int(input ("Enter the number: "))
+result = 0
+while True:
+    result = result + (number%10)
+    number = number // 10
+    if result < 10 and number == 0:
+        break
+    if number == 0:
+        number = result
+        result = 0
+print(result)        
+
+   
+
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+        
+        
+    
